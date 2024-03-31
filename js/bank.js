dca5e532b8fa17a06bfc591739db2bfc7ab071f5
P-Hemal <abdulhakim.hemal.99@gmail.com>
2024-03-31 12:26:51 +0600

    deposit section

---
+++ js/bank.js
@@ -0,0 +1,41 @@
+document.getElementById('btn-deposit').addEventListener('click', function(){
+    const depositField = document.getElementById('deposit-field');
+    const depositFieldString = depositField.value;
+    const newDepositAmount = parseFloat(depositFieldString);
+
+    const depositTotalElement = document.getElementById('deposit-total');
+    const previousDepositTotalString = depositTotalElement.innerText;
+    const previousDepositTotal = parseFloat(previousDepositTotalString);
+    const currentDepositTotal = previousDepositTotal + newDepositAmount;
+
+    const balanceTotalElement = document.getElementById('balance-total');
+    const previousBalanceTotalString = balanceTotalElement.innerText;
+    const previousBalanceTotal = parseFloat(previousBalanceTotalString);
+    const currentBalanceTotal = previousBalanceTotal + newDepositAmount
+    
+    balanceTotalElement.innerText = currentBalanceTotal
+    depositTotalElement.innerText = currentDepositTotal
+
+    depositField.value = '';
+})
+
+document.getElementById('btn-withdraw').addEventListener('click', function(){
+    const withdrawField = document.getElementById('withdraw-field');
+    const withdrawFieldString = withdrawField.value;
+    const newWithdrawAmount = parseFloat(withdrawFieldString);
+
+    const withdrawTotalElement = document.getElementById('withdraw-total');
+    const previousWithdrawTotalString = withdrawTotalElement.innerText;
+    const previousWithdrawTotal = parseFloat(previousWithdrawTotalString);
+    const currentWithdrawTotal = previousWithdrawTotal + newWithdrawAmount;
+
+    const balanceTotalElement = document.getElementById('balance-total');
+    const previousBalanceTotalString = balanceTotalElement.innerText;
+    const previousBalanceTotal = parseFloat(previousBalanceTotalString);
+    const currentBalanceTotal = previousBalanceTotal - newWithdrawAmount
+
+    balanceTotalElement.innerText = currentBalanceTotal
+    withdrawTotalElement.innerText = currentWithdrawTotal;
+
+    withdrawField.value = '';
+})
